7604692c6aa49594d1331378d78f4a02f2ceda6c
Jean Ponchon <jean.ponchon@novadiscovery.com>
2014-06-19 19:23:15 +0200

    test: Add test

--- test/docflux.test.js
+++ test/docflux.test.js
@@ -1,14 +1,49 @@
 /* jshint undef: false, unused: false */
 
-var docflux = require('../lib/docflux')
-var expect = require('expect.js')
-var join   = require('path').join
+var docflux  = require('../lib/docflux')
+var expect   = require('expect.js')
+var join     = require('path').join
+var concat   = require('concat-stream')
+var fs       = require('fs')
+var read     = require('fs').readFileSync
+var through2 = require('through2')
 
 
 function fixp(filename) {
   return join(__dirname, '/fixtures', filename);
 }
 
+function fread(filename) {
+  return fs.readFileSync(fixp(filename),'utf-8');
+}
+
 describe('docflux', function(){
-  it('should be tested ...')
+
+  it('json output validation', function(done) {
+    var isFirst = true;
+    var indent  = 2;
+    fs.createReadStream(fixp('source.js'))
+      .pipe(docflux())
+      .pipe(through2.obj(function(json, enc, next) {
+        var out = (isFirst ? '' : '\n') + JSON.stringify(json, null, indent);
+        isFirst = false;
+        next(null, out )
+      }))
+      .pipe(concat(function(data) {
+        expect(data).to.eql(fread('expect.json'))
+        done()
+      }))
+  })
+
+  it('markdown output validation', function(done) {
+    fs.createReadStream(fixp('source.js'))
+      .pipe(docflux())
+      .pipe(docflux.markdown())
+      .pipe(concat(function(data) {
+        expect(data).to.eql(fread('expect.md'))
+        done()
+      }))
+  })
+
+
 })
---
+++ test/fixtures/expect.md
@@ -0,0 +1,57 @@
+# MyClass()
+> A useful class
+
+This is exported (isExport must be true)
+
+**Jean Ponchon**
+
+--------------------------------------------------------------------------------
+
+## MyClass#createUser(name, groupId, [options])
+> Create new user
+
+Long description of this method with list and examples
+
+  - Do this
+  - And this
+
+Example:
+
+    var my = new MyClass();
+    my.createUser('Foo',['admin','staff'], { silent: true });
+
+**name** `{String}` 
+Username with some restrictions
+- Must be lower-case
+- Must be funny
+
+**groupId** `{String|String[]}` Group id or array of group id
+Put some markdow here too:
+
+    // Example
+
+**[options]** `{Object}` 
+Options are always optional
+but params description alignement is based on first line indentation
+
+**returns** `{Boolean}` 
+
+**Not funny (maybe)**
+
+**see**
+
+**MyClass**
+
+--------------------------------------------------------------------------------
+
+## anonymous([options])
+> Summary in a short and compact documentation
+
+Long description of this method
+
+**[options]** `{Object}` 
+
+**returns** `{Boolean}` 
+
+--------------------------------------------------------------------------------
+
